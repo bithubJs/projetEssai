40b5657526572c9a37b654aec2271b6efe7f2f94
INTI-0367 <INTI-0367@DESKTOP-0UOKIP4>
2017-05-24 14:26:43 +0200

    Client -> done
    Signed-off-by: INTI-0367 <INTI-0367@DESKTOP-0UOKIP4>

--- 54_Projet_e-commerce_SpringMVC_IoC_Hibernate/src/main/java/fr/adaming/dao/IClientDao.java
+++ 54_Projet_e-commerce_SpringMVC_IoC_Hibernate/src/main/java/fr/adaming/dao/IClientDao.java
@@ -6,12 +6,10 @@ import fr.adaming.model.Client;
 
 public interface IClientDao {
 	
-
 	public void addClient(Client c);
 	public List<Client> getAllClients();
 	public Client getClientById(int id);
 	public void updateClient(Client c);
 	public void deleteClient(Client c);
 	
-
 }
---
+++ 54_Projet_e-commerce_SpringMVC_IoC_Hibernate/src/main/java/fr/adaming/service/ClientServiceImpl.java
@@ -0,0 +1,49 @@
+package fr.adaming.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.dao.IClientDao;
+import fr.adaming.model.Client;
+
+@Service
+@Transactional
+public class ClientServiceImpl implements IClientService {
+
+	@Autowired
+	private IClientDao cDao;
+	
+	@Override
+	public void addClient(Client c) {
+
+		cDao.addClient(c);
+	}
+
+	@Override
+	public List<Client> getAllClients() {
+
+		return cDao.getAllClients();
+	}
+
+	@Override
+	public Client getClientById(int id) {
+
+		return cDao.getClientById(id);
+	}
+
+	@Override
+	public void updateClient(Client c) {
+
+		cDao.updateClient(c);
+	}
+
+	@Override
+	public void deleteClient(Client c) {
+
+		cDao.deleteClient(c);
+	}
+
+}
