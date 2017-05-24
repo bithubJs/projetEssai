b13875e5217d267a7ec7028329f1610bebe5bea6
INTI-0359 <INTI-0359@DESKTOP-N898QLU>
2017-05-24 12:39:54 +0200

    Class user & role Dieu

---
+++ 54_Projet_e-commerce_SpringMVC_IoC_Hibernate/src/main/java/fr/adaming/model/Role.java
@@ -0,0 +1,107 @@
+package fr.adaming.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_r")
+	private int id;
+
+	private String rolename;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id_u")
+	private User user;
+
+	/**
+	 * 
+	 */
+	public Role() {
+		super();
+	}
+
+	/**
+	 * @param rolename
+	 */
+	public Role(String rolename) {
+		super();
+		this.rolename = rolename;
+	}
+
+	/**
+	 * @param id
+	 * @param rolename
+	 */
+	public Role(int id, String rolename) {
+		super();
+		this.id = id;
+		this.rolename = rolename;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the rolename
+	 */
+	public String getRolename() {
+		return rolename;
+	}
+
+	/**
+	 * @param rolename
+	 *            the rolename to set
+	 */
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user
+	 *            the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", rolename=" + rolename + "]";
+	}
+
+}
